9449d15d4dbc8f4849925c65d09370fafd37ff7e
Nathan-hoareau <nathan.hoareau@epitech.eu>
2023-06-07 15:38:30 +0400

    we can finally build Qt

---
+++ include/Rect.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** EpiChess
+** File description:
+** Rect
+*/
+
+#pragma once
+#include <QGraphicsRectItem>
+
+class Rect : public QGraphicsRectItem {
+    public:
+        Rect() = default;
+        ~Rect() = default;
+};
--- main.cpp
+++ main.cpp
@@ -5,7 +5,24 @@
 ** main
 */
 
-int main ()
+#include <iostream>
+#include <QApplication>
+#include <QGraphicsScene>
+#include "Rect.hpp"
+#include <QGraphicsView>
+
+int main (int ac, char **av)
 {
-    return 0;
+    QApplication app(ac, av);
+
+    QGraphicsScene *scene = new QGraphicsScene();
+
+    Rect rect;
+    rect.setRect(0, 0, 100, 100);
+    rect.setBrush(Qt::white);
+
+    scene->addItem(&rect);
+    QGraphicsView *view = new QGraphicsView(scene);
+    view->show();
+    return app.exec();
 }
